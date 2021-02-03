e9dd4301fbdabfc01775c5ba8694a334bb5a9a58
Jorma Laaksonen <jorma.laaksonen@aalto.fi>
2021-02-03 16:20:00 +0200

    Added inverse rank fusion.

--- scripts/zettair-query-all.py
+++ scripts/zettair-query-all.py
@@ -3,8 +3,10 @@
 from bs4 import BeautifulSoup
 from os import path
 from paths import Paths
+from copy import copy
 
 import json, os, re
+import numpy as np
 
 paths = Paths()
 
@@ -65,31 +67,83 @@ with open(best_visual_path) as visual:
       vis[q] = []
     vis[q].append(i)
 
+
+def one_zet_result(q, n):
+    os.system('./zettair-query.py --index-prefix %s --n-best %d --query "%s" > zettair-query-output.tmp'
+              % (path.join(models_dir, 'setting-masked.zettair-index'), n, q))
+
+    res = []
+
+    with open('zettair-query-output.tmp', mode='r', encoding='utf-8') as query_output:
+      for line in query_output:
+        regex_search = re.search('^[0-9]+\. ([0-9]+)', line)
+        if regex_search:
+          res.append(regex_search.group(1))
+
+    os.system('rm -f zettair-query-output.tmp')
+    return res
+
+def fusion(r):
+  a = set()
+  for l, x in r:
+    for j in x:
+      a.add(j)
+  z = [ 1.0/(len(i[1])+1) for i in r]
+  b = {}
+  for i in a:
+    b[i] = copy(z)
+
+  # print(len(r[0][1]), len(r[1][1]), len(a), z)
+
+  for x in range(len(r)):
+    for i, j in enumerate(r[x][1]):
+      b[j][x] = 1.0/(i+1)
+
+  for i, l in b.items():
+    v = np.sum(l)
+    b[i] = v
+
+  s = sorted(b, reverse=True, key=lambda i:b[i])
+  #print(s)
+  #for i in s:
+  #  print(i, b[i])
+  return s
+    
 models_dir = paths.get('MODELS-DIR')
 
 avg_res = []
 
-for query_lang in ['en', 'de', 'fr', 'vi']:
+langs = ['en', 'de', 'fr', 'vi']
+langs_test = copy(langs)
+for i in range(len(langs)):
+  for j in range(i+1, len(langs)):
+    langs_test.append(langs[i]+'+'+langs[j])
+for i in range(len(langs)):
+  for j in range(i+1, len(langs)):
+    for k in range(j+1, len(langs)):
+      langs_test.append(langs[i]+'+'+langs[j]+'+'+langs[k])
+langs_test.append('en+de+fr+vi')
+      
+for query_lang in langs_test:
   avg = [0] * 8
-
+  ll = query_lang.split('+')
+  
   for topic_id in topic_descriptions:
-    if query_lang=='vi':
-      query_results = vis[topic_id][:50]
+    qres = []
+    for l in ll:
+      if l=='vi':
+        qr = vis[topic_id]
+      else:
+        topic_desc = topic_descriptions[topic_id][l]
+        qr = one_zet_result(topic_desc, 50)
+      qres.append((l, qr))
 
+    if len(qres)==1:
+      query_results = qres[0][1]
     else:
-      topic_desc = topic_descriptions[topic_id][query_lang]
-
-      os.system('./zettair-query.py --index-prefix %s --n-best 50 --query "%s" > zettair-query-output.tmp' % (path.join(models_dir, 'setting-masked.zettair-index'), topic_desc))
-
-      query_results = []
-
-      with open('zettair-query-output.tmp', mode='r', encoding='utf-8') as query_output:
-        for line in query_output:
-          regex_search = re.search('^[0-9]+\. ([0-9]+)', line)
-          if regex_search:
-            query_results.append(regex_search.group(1))
-
-      os.system('rm -f zettair-query-output.tmp')
+      query_results = fusion(qres)
+      
+    #print(topic_id, query_lang, len(query_results))
     
     query_matches = [topic_id in doc_topics[doc_id] for doc_id in query_results]
     
@@ -105,8 +159,12 @@ for query_lang in ['en', 'de', 'fr', 'vi']:
       recall_at[n] = float(len([match for match in first_n_matches if match])) / len(relevant_docs[topic_id]) \
                      if len(relevant_docs[topic_id]) > 0 else float(0)
     
-#    print('%d (%s)\n\tp@5: %.1f | p@10: %.1f | p@20: %.1f | p@50: %.1f\n\tr@5: %.1f | r@10: %.1f | r@20: %.1f | r@50: %.1f' % (topic_id, query_lang, precision_at[5], precision_at[10], precision_at[20], precision_at[50], recall_at[5], recall_at[10], recall_at[20], recall_at[50]))
-    print('%d\t(%s)\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f' % (topic_id, query_lang, precision_at[5], precision_at[10], precision_at[20], precision_at[50], recall_at[5], recall_at[10], recall_at[20], recall_at[50]))
+      #    print('%d (%s)\n\tp@5: %.1f | p@10: %.1f | p@20: %.1f | p@50: %.1f\n\tr@5: %.1f | r@10: %.1f | r@20: %.1f | r@50: %.1f' % (topic_id, query_lang, precision_at[5], precision_at[10], precision_at[20], precision_at[50], recall_at[5], recall_at[10], recall_at[20], recall_at[50]))
+      if False:
+        print('%d\t(%s)\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f'
+          % (topic_id, query_lang,
+             precision_at[5], precision_at[10], precision_at[20], precision_at[50],
+             recall_at[5], recall_at[10], recall_at[20], recall_at[50]))
 
     avg[0] +=  precision_at[5]
     avg[1] +=  precision_at[10]
