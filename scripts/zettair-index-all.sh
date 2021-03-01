704bc0f998228043687681065cbc2d40a3641f46
ymyt <umut.sulubacak@gmail.com>
2021-03-01 16:42:58 +0200

    Add search indices for new metadata setting variants

--- scripts/zettair-index-all.sh
+++ scripts/zettair-index-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for TREC_PATH in $(ls *.trec); do
+for TREC_PATH in $(ls ../data/{setting-original,setting-masked}.*trec); do
   ZIDX_PRFX=${TREC_PATH%.trec}.zettair-index
   
-  rm ${ZIDX_PRFX}.*
-  
   zet --index --filename ${ZIDX_PRFX} --big-and-fast -t TREC --index ${TREC_PATH}
+  
+  mv ${ZIDX_PRFX}* ../models
 done
 
 echo "...all done!"
