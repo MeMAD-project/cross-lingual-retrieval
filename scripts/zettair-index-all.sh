a5d76f98fdc30a5318eecebea9c2cc827df1edab
ymyt <umut.sulubacak@gmail.com>
2021-03-02 15:20:21 +0200

    Add search indices built only from images with explicit relevance annotations (qrels-only)

--- scripts/zettair-index-all.sh
+++ scripts/zettair-index-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for TREC_PATH in $(ls ../data/{setting-original,setting-masked}.*trec); do
+for TREC_PATH in $(ls ../data/setting*.trec); do
   ZIDX_PRFX=${TREC_PATH%.trec}.zettair-index
   
   zet --index --filename ${ZIDX_PRFX} --big-and-fast -t TREC --index ${TREC_PATH}
